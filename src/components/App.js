c0140e1f29b9236dca1bbde79b8bb06f52ec3bf8
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2022-03-25 04:57:21 -0300

    layout rota 1

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import TelaSelecaoFilme from "./TelaSelecaoFilme";
+
+export default function App(){
+    return(
+        <TelaSelecaoFilme />
+    )
+
+}
---
+++ src/components/TelaSelecaoFilme.js
@@ -0,0 +1,39 @@
+import Header from "./Header";
+
+export default function TelaSelecaoFilme(){
+    return(
+        <>
+        <Header />
+        <section className="selecao">Selecione o filme</section>
+        <section className="opcoes-filmes">
+            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>
+            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>
+            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>
+            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>
+            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>
+            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>
+            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>            <div className="filme">
+                <img src="./assets/filme-2067.png" alt="filme 2067"/>
+            </div>
+        </section>
+        </>
+    )
+}
